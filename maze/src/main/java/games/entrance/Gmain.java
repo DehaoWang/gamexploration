31a84ea6b6fecee132fc0a1f18abfe2975ad4ac2
王德浩 <wangdehao@meituan.com>
2018-01-25 11:06:41 +0800

    maze

---
+++ maze/src/main/java/games/entrance/Gmain.java
@@ -0,0 +1,13 @@
+package games.entrance;
+
+
+/**
+ * Created by wangdehao on 18/1/15.
+ */
+public class Gmain {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("MAZE");
+
+    }
+}
